a39865653a8cc75847c77c9386058182741ed9c5
Ancelotow <ancelotow@gmail.com>
2023-01-09 22:54:44 +0100

    .feat: add loadingAlert during loading

--- swish-deliverer/HomeViewController.swift
+++ swish-deliverer/HomeViewController.swift
@@ -15,6 +15,7 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var forgotPasswordButton: UIButton!
     @IBOutlet weak var signInButton: UIButton!
     let connectionService: ConnectionService = ConnectionDbService()
+    var loadingAlert: UIAlertController? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,28 +30,53 @@ class HomeViewController: UIViewController {
         self.signInButton.setTitle(NSLocalizedString(LocalizedStringKeys.sign_in.rawValue, comment: ""), for: .normal)
     }
     
-    func showErrorMessage(_ message: String) {
+    func showErrorAlertWithMessage(_ message: String) {
         let alert = UIAlertController(title: "Invalide", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Fermer", style: .cancel))
         self.present(alert, animated: true)
+    }
+    
+    func showLoadingAlert() {
+        self.loadingAlert = UIAlertController(title: nil, message: "Connexion...", preferredStyle: .alert)
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.medium
+        loadingIndicator.startAnimating();
+        self.loadingAlert!.view.addSubview(loadingIndicator)
+        present(self.loadingAlert!, animated: true, completion: nil)
+    }
+    
+    func dismissLoadingAlert(_ completion: @escaping () -> Void) {
+        guard let alert = self.loadingAlert else {
+            return
+        }
+        alert.dismiss(animated: true, completion: completion)
+        self.loadingAlert = nil
     }
 
     @IBAction func signIn(_ sender: Any) {
         guard let login = loginTextField.text,
               let password = passwordTextField.text,
               !login.isEmpty, !password.isEmpty  else {
-            self.showErrorMessage("L'identifiant et le mot de passes sont obligatoires")
+            self.showErrorAlertWithMessage("L'identifiant et le mot de passes sont obligatoires")
             return
         }
+        self.showLoadingAlert()
         connectionService.connection(login: login, password: password) { msg, err in
             guard err == nil else {
                 DispatchQueue.main.async {
-                    let message = (err as? NSError)?.localizedFailureReason ?? "Erreur inconnue"
-                    self.showErrorMessage(message)
+                    self.dismissLoadingAlert() {
+                        let message = (err as? NSError)?.localizedFailureReason ?? "Erreur inconnue"
+                        self.showErrorAlertWithMessage(message)
+                    }
                 }
                 return
             }
-            print(Session.getSession()!.firstname)
+            DispatchQueue.main.async {
+                self.dismissLoadingAlert() {
+                    print(Session.getSession()!.firstname)
+                }
+            }
         }
     }
     
